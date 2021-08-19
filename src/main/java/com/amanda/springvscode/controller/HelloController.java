ed982a03491bff496fe03078cf170260044734ab
Amanda Camara <camara.amanda@gmail.com>
2021-08-19 07:24:16 -0300

    spring vsCode

---
+++ src/main/java/com/amanda/springvscode/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.amanda.springvscode.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * HelloController
+ */
+@RestController
+
+
+public class HelloController {
+
+    @GetMapping(value="/")
+    public String getMethodName() {
+        return "Olá mundo VSCode!";
+    }
+    
+
+    
+}
